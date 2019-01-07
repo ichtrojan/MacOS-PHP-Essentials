96911a376965b9907925e095bc343d8daaa74387
Michael Okoh <ordrizzy@gmail.com>
2019-01-07 09:48:19 +0100

    change composer installation to brew

--- script.sh
+++ script.sh
@@ -1,27 +1,11 @@
 #!/bin/sh
 
-EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
-
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-then
-    >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
-    exit 1
-fi
-
-php composer-setup.php --quiet
-RESULT=$?
-rm composer-setup.php
-exit $RESULT
-
-cp composer.phar /usr/local/bin/composers
-
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install php71
+brew install php
 
 brew install mysql
 
 brew install nginx
+
+brew install composer
